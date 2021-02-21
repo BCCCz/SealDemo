c531c6e22f46177f9e0d468ddf71d0b6c432757a
BC <455288561@qq.com>
2021-02-22 00:30:07 +0800

    start
    start

---
+++ CMakeFiles/sealdemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sealdemo.dir/sealdemo.cpp.o"
+  "CMakeFiles/sealdemo.dir/sealdemo.cpp.o.d"
+  "sealdemo"
+  "sealdemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sealdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
